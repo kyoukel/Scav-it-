d9504530cc4b98e7df71d236e60e40ca41c044c7
dimstar <kylermagee@gmail.com>
2018-03-26 18:18:22 -0700

    fixing distance

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -233,7 +233,7 @@ function distance(lat1, lon1, lat2, lon2, unit) {
 	dist = Math.acos(dist)
 	dist = dist * 180/Math.PI
 	dist = dist * 60 * 1.1515
-	if (unit=="K") { dist = (dist * 1.609344)/10 }
+	if (unit=="K") { dist = (dist * 1.609344) }
 	if (unit=="N") { dist = dist * 0.8684 }
 	return dist
 }
